ddccdf47cf4221c3473dccc83c0caf06a8449d37
Amit Kumar Gupta <webstorm404@gmail.com>
2025-03-09 16:10:12 +0530

    day2: solves call polyfill

---
+++ function_polyfills/call_polyfill.js
@@ -0,0 +1,35 @@
+/*
+The Function.prototype.call() method calls the function with a given this value and arguments provided individually.
+
+Source: Function.prototype.call() - JavaScript | MDN
+
+Implement your own Function.prototype.call without calling the native call method. To avoid overwriting the actual Function.prototype.call, implement the function as Function.prototype.myCall.
+
+Examples
+
+function multiplyAge(multiplier = 1) {
+  return this.age * multiplier;
+}
+
+const mary = {
+  age: 21,
+};
+
+const john = {
+  age: 42,
+};
+
+multiplyAge.myCall(mary); // 21
+multiplyAge.myCall(john, 2); // 84
+*/
+
+/**
+ * @param {any} thisArg
+ * @param {...*} argArray
+ * @return {any}
+ */
+Function.prototype.myCall = function (thisArg, ...argArray) {
+    if(!thisArg) thisArg = {};
+    thisArg.fn = this;
+    return thisArg.fn(...argArray)
+  };
