26262cc2e37949a823579de5789c83a35d465bf4
Amit Kumar Gupta <webstorm404@gmail.com>
2025-03-14 15:18:21 +0530

    solves remove element - optimized

--- dsa-250/01-array-hashing/07-remove-element.js
+++ dsa-250/01-array-hashing/07-remove-element.js
@@ -73,3 +73,32 @@ var removeElement = function(nums, val) {
 
     return nums.length;
 };
+
+
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+// var removeElement = function(nums, val) {
+//     for(let i = 0; i < nums.length; i++){
+//         if(nums[i] === val) {
+//             nums.splice(i, 1);
+//             i--;
+//         }
+//     };
+
+//     return nums.length;
+// };
+
+// TC: O(n), SC: O(1)
+var removeElement = function(nums, val) {
+    let k = 0;
+    for(let i = 0; i < nums.length; i++) {
+        if(nums[i] !== val) {
+            nums[k] = nums[i];
+            k++;
+        }
+    };
+    return k;
+}
